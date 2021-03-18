a5aa067d8c4169ef8e2e981c7052d15335891405
JekaNoah007 <zhmenik1988@mail.ru>
2021-03-18 19:05:14 +0600

    lesson4.dart

---
+++ lesson6.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Как вас зовут?");
+  String _name = stdin.readLineSync();
+  stdout.write("Сколько Вам лет?");
+  String _age = stdin.readLineSync();
+  print("Ваше имя $_name вам $_age");
+
+  stdout.write("Введите номер: ");
+  int number = int.parse(stdin.readLineSync());
+  if (number % 2 == 0) {
+    print('Число $number четное');
+  } else {
+    print('Число $number нечетное');
+  }
+}
