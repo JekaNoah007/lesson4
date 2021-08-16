e07a24b1feaff8c46c51d5b9948e95f158855b38
JekaNoah007 <JekaNoah007@gmail.com>
2021-08-16 19:41:05 +0600

    adding a task

--- lesson6.dart
+++ lesson6.dart
@@ -2,13 +2,13 @@ import 'dart:io';
 
 void main() {
   stdout.write("Как вас зовут?");
-  String _name = stdin.readLineSync();
+  String _name = stdin.readLineSync()!;
   stdout.write("Сколько Вам лет?");
-  String _age = stdin.readLineSync();
+  String _age = stdin.readLineSync()!;
   print("Ваше имя $_name вам $_age");
 
   stdout.write("Введите номер: ");
-  int number = int.parse(stdin.readLineSync());
+  int number = int.parse(stdin.readLineSync()!);
   if (number % 2 == 0) {
     print('Число $number четное');
   } else {
---
+++ lesson6.task3.dart
@@ -0,0 +1,11 @@
+main() {
+  List a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  for (int i = 0; i < a.length; i++) {
+    // print(a[i]);
+    if (a[i] < 5) {
+      print(a[i]);
+//     } else
+//       print(a[i]);
+    }
+  }
+}
